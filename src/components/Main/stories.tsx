0cc89b0f98cb31618b8145b27b7e533829c848f8
Déborah Malheiro <deblima_@hotmail.com>
2021-06-21 16:39:23 -0300

    Usando Storybook Essentials e Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,23 +1,19 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from './index'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', '111111111111')}
-    description={text('Description', 'Description teste')}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'kjshdkashds',
+  description: 'shsdkfnhsdujs'
+}
 
-export const Secondary: Story = () => (
-  <Main
-    title={text('Title', '22222222222')}
-    description={text('Description', 'Description teste')}
-  />
-)
+export const Default: Story = (args) => <Main {...args} />
